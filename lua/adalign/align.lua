4c371af8cb4caed80468857627fd2bb30cd5a4f6
Rustum Zia <ziarustum@gmail.com>
2026-03-12 01:15:04 +0100

    refactor: nvim_buf_add_highlight is deprecated. replace with vim.hl.range.

--- lua/adalign/align.lua
+++ lua/adalign/align.lua
@@ -1,13 +1,12 @@
 local M = {}
 local api = vim.api
+local adalign = require "adalign.util"
+
 
 api.nvim_set_hl(0, "AdalignInsertSpace",  { link = "Substitute", default = true })
 api.nvim_set_hl(0, "AdalignMatch",        { link = "Search",     default = true })
 api.nvim_set_hl(0, "AdalignLeadingMatch", { link = "IncSearch",  default = true })
 api.nvim_set_hl(0, "AdalignDeleteSpace",  { link = "Substitute", default = true })
-
-local api = vim.api
-local adalign = require "adalign.util"
 
 function M.command(ctx)
     local startline = ctx.line1 - 1 --to 0-based index
@@ -47,13 +46,12 @@ function M.preview(ctx, preview_ns, preview_buf)
         for i = 1, #lines do
             -- highlight inserts
             if inserts[i] then
-                api.nvim_buf_add_highlight(
+                vim.hl.range(
                     buffer,
                     preview_ns,
                     'AdalignInsertSpace',
-                    startline + i-1,
-                    inserts[i].start-1,
-                    inserts[i].start-1 + #inserts[i].text
+                    { startline + i-1, inserts[i].start-1 },
+                    { startline + i-1, inserts[i].start-1 + #inserts[i].text }
                 )
             end
 
@@ -61,14 +59,13 @@ function M.preview(ctx, preview_ns, preview_buf)
             local is_leading = matches.columns[i] == targetcol
             if matches.indices[i] then
                 local chars_inserted = inserts[i] and #inserts[i].text or 0
-                api.nvim_buf_add_highlight(
+                vim.hl.range(
                     buffer,
                     preview_ns,
                     is_leading and 'AdalignLeadingMatch' or 'AdalignMatch',
-                    startline + i-1,
                     -- matches indices don't take into account new inserts
-                    matches.indices[i] + chars_inserted,
-                    matches.ends[i] + chars_inserted
+                    { startline + i-1, matches.indices[i] + chars_inserted },
+                    { startline + i-1, matches.ends[i] + chars_inserted }
                 )
             end
         end
